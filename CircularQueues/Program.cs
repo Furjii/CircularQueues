bd4fbf2c19a7b7e098dd45162250ed5eb2175b68
Furji UMY <furji.ft21@mail.umy.ac.id>
2022-12-15 16:11:49 +0700

    add variabel and array

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -8,6 +8,8 @@ namespace CircularQueues
 {
     class Queues
     {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
         static void Main(string[] args)
         {
         }
